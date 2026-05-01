3ed428290d7378d4d63197acd83b5f40e8850d22
Sabyasachi Purkayastha <route2sabya@gmail.com>
2026-05-01 12:32:01 +0530

    Add sitemap.xml, robots.txt and meta description for SEO

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import threading
 import time
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
 from scorer import get_scorer
 
 app    = Flask(__name__)
@@ -74,6 +74,25 @@ def api_stats():
             "last":  s.session_order[-1][0],
         },
     })
+
+
+@app.route("/sitemap.xml")
+def sitemap():
+    xml = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://lottery-sambad-checker.fly.dev/</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>"""
+    return Response(xml, mimetype="application/xml")
+
+
+@app.route("/robots.txt")
+def robots():
+    txt = "User-agent: *\nAllow: /\nSitemap: https://lottery-sambad-checker.fly.dev/sitemap.xml"
+    return Response(txt, mimetype="text/plain")
 
 
 if __name__ == "__main__":
